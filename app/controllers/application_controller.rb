ef16b39052cc09b37f9f99fcd5e7d837f3e31b9a
”taro <groveleffect@gmail.com>
2019-12-09 09:01:18 +0900

    プロフィール画像の投稿機能を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile_image])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile_image])
     end
 end
