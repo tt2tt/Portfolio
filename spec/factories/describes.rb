1b2f45fe95b37849f531c897d4548dd836cf2789
”taro <groveleffect@gmail.com>
2019-12-13 16:18:31 +0900

    DescribeのE2Eテストの追加

--- spec/factories/describes.rb
+++ spec/factories/describes.rb
@@ -4,4 +4,10 @@ FactoryBot.define do
     content {"仮コンテント"}
     images { [ Rack::Test::UploadedFile.new(Rails.root.join('spec/factories/test.jpg'), 'spec/factories/test.jpg') ] }
   end
+
+  factory :second_describe, class: Describe do
+    title {"仮説明"}
+    content {"仮内容"}
+    images { [ Rack::Test::UploadedFile.new(Rails.root.join('spec/factories/test.jpg'), 'spec/factories/test.jpg') ] }
+  end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,4 +6,12 @@ FactoryBot.define do
     password_confirmation {'AAAAAA'}
     profile_image {''}
   end
+
+  factory :second_user, class: User do
+    name {"仮ユーザー2"}
+    email {"bbb@bbb.com"}
+    password {'BBBBBB'}
+    password_confirmation {'BBBBBB'}
+    profile_image {''}
+  end
 end
