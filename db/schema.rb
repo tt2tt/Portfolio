e64aeb5ce8ea3c0f967620ea111bfa8d25bad0c6
”taro <groveleffect@gmail.com>
2019-12-15 21:05:31 +0900

    Likeモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_15_113139) do
+ActiveRecord::Schema.define(version: 2019_12_15_114349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,16 @@ ActiveRecord::Schema.define(version: 2019_12_15_113139) do
     t.bigint "user_id"
     t.integer "likes_counts"
     t.index ["user_id"], name: "index_describes_on_user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "describe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["describe_id"], name: "index_likes_on_describe_id"
+    t.index ["user_id", "describe_id"], name: "index_likes_on_user_id_and_describe_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +52,6 @@ ActiveRecord::Schema.define(version: 2019_12_15_113139) do
   end
 
   add_foreign_key "describes", "users"
+  add_foreign_key "likes", "describes"
+  add_foreign_key "likes", "users"
 end
