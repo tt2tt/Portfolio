bbebb846eb18f71b19f396e375a4d594cfefdd92
”taro <groveleffect@gmail.com>
2019-12-20 10:25:22 +0900

    説明へコメントがあった際のメール通知機能の追加

---
+++ app/mailers/comment_mailer.rb
@@ -0,0 +1,9 @@
+class CommentMailer < ApplicationMailer
+  def comment_mail(mail, comment)
+    @mail = mail
+    @comment = comment
+    
+    mail to: @mail, subject: "あなたの投稿にコメントがありました。"
+  end
+
+end
