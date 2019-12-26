9a2d75c0888eb78f65fba8e8f8eff16c26b1479a
”taro <groveleffect@gmail.com>
2019-12-26 16:06:02 +0900

    シードデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# user
+20.times do |n|
+  name = Faker::Name.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(
+               name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               )
+end
+
+# describe
+20.times do |n|
+  title = [*'A'..'Z'][rand(26)]
+  content = '仮説明'
+  user_id =  rand(1..2)
+  Describe.create!(title: title,
+               content: content,
+               user_id: user_id,
+               )
+end
+
+# comment
+20.times do |n|
+  content = '仮コメント'
+  user_id =  rand(1..2)
+  describe_id = rand(1..5)
+  Comment.create!(
+               content: content,
+               user_id: user_id,
+               describe_id: describe_id,
+               )
+end
+
+# like
+Like.create(user_id: 1, describe_id: 1)
+Like.create(user_id: 2, describe_id: 1)
+Like.create(user_id: 2, describe_id: 2)
+Like.create(user_id: 2, describe_id: 3)
+Like.create(user_id: 3, describe_id: 3)
