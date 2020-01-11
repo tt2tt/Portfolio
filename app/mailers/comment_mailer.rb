764929cdf8e2e47b1cd9526cbeca2a361574911a
”taro <groveleffect@gmail.com>
2020-01-11 10:43:35 +0900

    メール送信の不具合を修正

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,9 +1,10 @@
 class CommentMailer < ApplicationMailer
+
   def comment_mail(mail, comment)
     @mail = mail
     @comment = comment
 
-    mail to: @mail, subject: "あなたの投稿にコメントがありました。"
+    mail to: "#{@mail}", subject: "あなたの投稿にコメントがありました。"
   end
 
 end
