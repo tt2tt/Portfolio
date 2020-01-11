d4edf94a51c40a51b87a86fac37b09398d4f72b2
”taro <groveleffect@gmail.com>
2020-01-11 13:54:11 +0900

    本番環境でのメール送信機能の不具合修正2

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module BrainShare
     config.active_record.default_timezone = :local
 
     config.action_view.embed_authenticity_token_in_remote_forms = true
+
+    config.action_mailer.delivery_method = :ses
+    config.action_mailer.default_url_options = { host: 'sharebrain.net' }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,9 +64,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "brain_share_#{Rails.env}"
 
   # config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'sharebrain.net' }
-  config.mailer_sender = 'sharebrain.net <noreply@sharebrain.net>'
-  config.action_mailer.delivery_method = :ses
+  # config.action_mailer.delivery_method = :ses
+  # config.action_mailer.default_url_options = { host: 'sharebrain.net' }
+  # config.mailer_sender = 'sharebrain.net <noreply@sharebrain.net>'
   config.action_mailer.raise_delivery_errors = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
