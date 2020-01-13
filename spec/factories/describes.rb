b6f5f9ad24b8427c3eb6e2e90e857b0118d7c4e5
”taro <groveleffect@gmail.com>
2020-01-13 14:31:11 +0900

    テストの不具合を修正

--- spec/factories/describes.rb
+++ spec/factories/describes.rb
@@ -2,18 +2,18 @@ FactoryBot.define do
   factory :describe, class: Describe do
     title {"仮タイトル"}
     content {"仮コンテント"}
-    images { [ Rack::Test::UploadedFile.new(Rails.root.join('spec/factories/test.jpg'), 'spec/factories/test.jpg') ] }
+    images {""}
   end
 
   factory :second_describe, class: Describe do
     title {"仮説明"}
     content {"仮内容"}
-    images { [ Rack::Test::UploadedFile.new(Rails.root.join('spec/factories/test.jpg'), 'spec/factories/test.jpg') ] }
+    images {""}
   end
 
   factory :third_describe, class: Describe do
     title {"AAA"}
     content {"AAA"}
-    images { [ Rack::Test::UploadedFile.new(Rails.root.join('spec/factories/test.jpg'), 'spec/factories/test.jpg') ] }
+    images {""}
   end
 end
--- spec/models/describe_spec.rb
+++ spec/models/describe_spec.rb
@@ -2,17 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Describe, type: :model do
   current_user = User.create(name: '仮ユーザー', email: "aaa@aaa.com", password: "AAAAAA", password_confirmation: "AAAAAA")
-  let(:image_path) { Rails.root.join('spec/factories/test.jpg') }
-  let(:image) { Rack::Test::UploadedFile.new(image_path) }
 
 
   it "titleが空ならバリテーションが通らない" do
-    describe = current_user.describes.build(title: '', content: '仮コンテント', images: [image])
+    describe = current_user.describes.build(title: '', content: '仮コンテント', images: '')
     expect(describe).not_to be_valid
   end
 
   it "titleが101文字以上ならバリテーションが通らない" do
-    describe = current_user.describes.build(title: 'a' * 101, content: '仮コンテント', images: [image])
+    describe = current_user.describes.build(title: 'a' * 101, content: '仮コンテント', images: '')
     expect(describe).not_to be_valid
   end
 
@@ -22,7 +20,7 @@ RSpec.describe Describe, type: :model do
   end
 
   it "全ての条件を満たしていればバリテーションが通る" do
-    describe = current_user.describes.build(title: '仮タイトル', content: '仮コンテント', images: [image])
+    describe = current_user.describes.build(title: '仮タイトル', content: '仮コンテント', images: '')
     expect(describe).to be_valid
   end
 end
