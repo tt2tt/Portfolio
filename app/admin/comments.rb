db43640ab2f66980688c0ed5fdfff57ec4120830
”taro <groveleffect@gmail.com>
2020-02-06 21:03:19 +0900

    管理者画面の作成

---
+++ app/admin/comments.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Comment do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :content, :user_id, :describe_id, :comment_number, :reply_comment_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:content, :user_id, :describe_id, :comment_number, :reply_comment_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
---
+++ app/admin/describes.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Describe do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :title, :content, :images, :user_id, :likes_count, :comments_count, :original_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:title, :content, :images, :user_id, :likes_count, :comments_count, :original_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
